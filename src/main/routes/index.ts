c4a77ac8df212590f42aac3f93aefd0e6fe76a23
Caio Braga <caioaugustobrg@gmail.com>
2024-04-11 12:16:39 -0300

    cors

--- src/main/routes/index.ts
+++ src/main/routes/index.ts
@@ -4,5 +4,8 @@ import UserRouter from './user.routes'
 const router = express.Router()
 
 router.use('/user', UserRouter)
-
+router.get('/', (req,res) => {
+    res.type('text/plain')
+    res.send('main')
+})
 export default router
