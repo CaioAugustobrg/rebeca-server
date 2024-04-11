d19e674677a291376028237c669d8f5e6209ea3b
Caio Braga <caioaugustobrg@gmail.com>
2024-04-11 13:20:04 -0300

    cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import router from '../routes';
@@ -6,35 +6,21 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
-// Configure CORS
+// Configurar CORS
 app.use(cors({
-    origin: true,
-   // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed HTTP methods
-   // allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
-    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-  }));
-  
+    origin: 'https://rebbeca-birthday.vercel.app', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], 
+    credentials: true, 
+}));
 
-  app.use((req, res, next) => {
-    res.header(
-      "Access-Control-Allow-Origin",
-      "https://rebbeca-birthday.vercel.app, https://rebbeca-birthday.vercel.app"
-    );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.header(
-      "Access-Control-Allow-Origin",
-      "X-Requested-With, Content-Type, Authorization"
-    );
-    res.header(
-      "Access-Control-Allow-Methods",
-      "GET,PUT,PATCH,POST,DELETE,OPTIONS"
-    );
-  
+app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header("Access-Control-Allow-Origin", "https://rebbeca-birthday.vercel.app");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
     next();
-  });
+});
+
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser('keyboard cat'));
