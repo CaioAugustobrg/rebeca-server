78dd7b363ecdbc120f7b0b9735e9bcbc8986a682
Caio Braga <caioaugustobrg@gmail.com>
2024-04-11 10:24:26 -0300

    cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -6,11 +6,12 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
+// Configure CORS
 app.use(cors({
-    origin: 'https://rebbeca-birthday.vercel.app', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
+    origin: 'https://rebbeca-birthday.vercel.app', // Specify the front-end origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
+    credentials: true, // Allow credentials (cookies, authorization headers, etc.)
 }));
 
 app.use(express.json());
