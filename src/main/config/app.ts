1a1e54f26f9762a38a6cb4329fad6a80b3bdd707
Caio Braga <caioaugustobrg@gmail.com>
2024-04-11 10:43:46 -0300

    cors

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -8,7 +8,7 @@ const app = express();
 
 // Configure CORS
 app.use(cors({
-    origin: 'https://rebbeca-birthday.vercel.app', // Specify the front-end origin
+    origin: 'https://rebbeca-birthday-caio-augusto-da-silva-bragas-projects.vercel.app/', // Specify the front-end origin
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Specify allowed HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Specify allowed headers
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
@@ -16,11 +16,11 @@ app.use(cors({
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://rebbeca-birthday.vercel.app"
+    "https://rebbeca-birthday-caio-augusto-da-silva-bragas-projects.vercel.app/"
   );
   res.header(
     "Access-Control-Allow-Headers",
-    "https://rebbeca-birthday.vercel.app"
+    "https://rebbeca-birthday-caio-augusto-da-silva-bragas-projects.vercel.app/"
   );
   res.header(
     "Access-Control-Allow-Origin",
